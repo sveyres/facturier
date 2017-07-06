613691b834c3efc1cc98b5c0a7282f1f11e495e7
Sylvie <sylvie.veyres@gmail.com>
2017-07-06 17:12:20 +0200

    add list-client url/view/template

--- app_facturier/urls.py
+++ app_facturier/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     url(r'^(?P<slug>[\w-]+)/edit$', ProposalUpdateView.as_view(), name="proposal-validate"),
     url(r'^(?P<slug>[\w-]+)/delete$', ProposalDeleteView.as_view(), name="proposal-delete"),
     url(r'^client/new/$', ClientCreateView.as_view(), name="client-create"),
+    url(r'^liste/clients/$', ClientListView.as_view(), name="list-clients"),
     url(r'^line/new/$', LineCreateView.as_view(), name="line-create"),
-    url(r'^(?P<slug>[\w-]+)$', ProposalDetailView.as_view(), name="proposal-detail"),
     url(r'^(?P<ref>[\w-]+)/change$', change_status, name="proposal-change"),
     url(r'^(?P<ref>[\w-]+)/arch$', arch_proposal, name="proposal-arch"),
     url(r'^tresorerie/$', TresListView.as_view(), name="tresorerie"),
     url(r'^archives/$', ArchListView.as_view(), name="archives"),
+
 ]
--- app_facturier/views.py
+++ app_facturier/views.py
@@ -11,6 +11,46 @@ import datetime
 def homepage(request):
     return render(request, 'homepage.html')
 
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = "__all__"
+    template_name = "app_facturier/client_create.html"
+    success_url = reverse_lazy('proposal-create')
+
+
+class ClientListView(ListView):
+    model = Client
+    context_object_name = "clients"
+
+
+
+
+class ProposalDetailView(DetailView):
+    model = Proposal
+    slug_field = "ref"
+    template_name = "app_facturier/proposal_detail.html"
+
+class ProposalCreateView(CreateView):
+    model = Proposal
+    fields = ('client', 'profile', 'status', 'ref')
+    template_name = "app_facturier/proposal_create.html"
+    success_url = reverse_lazy('tdb')
+
+    def get_context_data(self, form=None):
+        context = CreateView.get_context_data(self)
+        context["line_formset"] = lineInlineFormset()
+        return context
+
+    def form_valid(self, form):
+        line_resp = CreateView.form_valid(self, form)
+        line_formset = lineInlineFormset(self.request.POST, instance=self.object)
+
+        if line_formset.is_valid():
+            line_formset.save()
+
+        return line_resp
+
 class ProposalListView(ListView):
     # context_object_name = "proposals"
     model = Proposal
@@ -21,6 +61,30 @@ class ProposalListView(ListView):
         context['devEnCours'] = Proposal.objects.filter(status = "DEVEC")
         context['factEnCours'] = Proposal.objects.filter(status = "FACEC")
         return context
+
+class ProposalUpdateView(UpdateView):
+    model = Proposal
+    slug_field = "ref"
+    fields = "__all__"
+
+    def get_success_url(self):
+        return reverse("proposal-detail", kwargs={'slug': self.object.ref})
+
+class ProposalDeleteView(DeleteView):
+    model = Proposal
+    slug_field = "ref"
+    success_url = reverse_lazy('tdb')
+
+
+
+
+class LineCreateView(CreateView):
+    model = Line
+    fields = "__all__"
+    template_name = "app_facturier/line_create.html"
+    success_url = reverse_lazy('proposal-create')
+
+
 
 class TresListView(ListView):
     model = Proposal
@@ -49,56 +113,10 @@ class ArchListView(ListView):
         context['factPayee'] = Proposal.objects.filter(status = "FPAYE")
         return context
 
-class ProposalDetailView(DetailView):
-    model = Proposal
-    slug_field = "ref"
-    template_name = "app_facturier/proposal_detail.html"
-
-class ProposalCreateView(CreateView):
-    model = Proposal
-    fields = ('client', 'profile', 'status', 'ref')
-    template_name = "app_facturier/proposal_create.html"
-    success_url = reverse_lazy('tdb')
-
-    def get_context_data(self, form=None):
-        context = CreateView.get_context_data(self)
-        context["line_formset"] = lineInlineFormset()
-        return context
-
-    def form_valid(self, form):
-        line_resp = CreateView.form_valid(self, form)
-        line_formset = lineInlineFormset(self.request.POST, instance=self.object)
-
-        if line_formset.is_valid():
-            line_formset.save()
-
-        return line_resp
-
-class ClientCreateView(CreateView):
-    model = Client
-    fields = "__all__"
-    template_name = "app_facturier/client_create.html"
-    success_url = reverse_lazy('proposal-create')
-
-class LineCreateView(CreateView):
-    model = Line
-    fields = "__all__"
-    template_name = "app_facturier/line_create.html"
-    success_url = reverse_lazy('proposal-create')
-
-class ProposalUpdateView(UpdateView):
-    model = Proposal
-    slug_field = "ref"
-    fields = "__all__"
-
-    def get_success_url(self):
-        return reverse("proposal-detail", kwargs={'slug': self.object.ref})
 
 
-class ProposalDeleteView(DeleteView):
-    model = Proposal
-    slug_field = "ref"
-    success_url = reverse_lazy('tdb')
+
+
 
 
 def change_status(request, ref):
@@ -107,10 +125,11 @@ def change_status(request, ref):
     prop.status = "FACEC"
     prop.date_acceptance = now
     prop.save()
-    context = {
-        "proposal" : prop
-    }
-    return render(request, "app_facturier/proposal_detail.html", context)
+    # context = {
+    #     "ref" : prop.ref
+    # }
+    return redirect('tdb')
+    # return render(request, "app_facturier/proposal_detail.html", context)
 
 def arch_proposal(request, ref):
     now = datetime.datetime.now()
